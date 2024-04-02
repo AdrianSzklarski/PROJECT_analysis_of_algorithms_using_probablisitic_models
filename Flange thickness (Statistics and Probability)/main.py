aeda1a154f30794f9bf7eee7692e5908868a5471
Cetus <adrian_it@o2.pl>
2024-04-02 16:55:16 +0200

    Added first step - var a

---
+++ Flange thickness (Statistics and Probability)/main.py
@@ -0,0 +1,10 @@
+"""The main programme for running applications
+    @AdrianSzklarski, 04.2024"""
+
+
+class Task:
+    """Runs all modules of this program"""
+
+
+if __name__ == 'main':
+    resume = Task()
---
+++ Flange thickness (Statistics and Probability)/module/taks_one.py
@@ -0,0 +1,10 @@
+"""The programme prepares data for the calculation as a sorted list"""
+
+
+class TaskOne:
+    def __init__(self, empty_list):
+        self.empty_list = empty_list
+
+if __name__ == '__main__':
+    a = []
+    TaskOne(a)
